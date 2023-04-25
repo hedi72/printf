1ef817a3317ed01fe4c97a5e30b506423771c179
hedi72 <heditaieb72@gmail.com>
2023-04-25 03:45:34 -0700

    fixed: put

--- _put.c
+++ _put.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 
 /**
- * _puts - affiche une chaîne de caractères avec un retour à la ligne
- * @str: la chaîne de caractères à afficher
+ * _puts - prints a string with newline
+ * @str: the string to print
  *
- * Retour: void
+ * Return: void
  */
+
 int _puts(char *str)
 {
 char *a = str;
@@ -14,13 +15,12 @@ while (*str)
 _putchar(*str++);
 return (str - a);
 }
-
 /**
- * _putchar - écrit le caractère c dans stdout
- * @c: Le caractère à afficher
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Retour: En cas de succès, retourne 1.
- * En cas d'erreur, -1 est retourné, et errno est défini correctement.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(int c)
 {
@@ -28,7 +28,7 @@ static int i;
 static char buf[OUTPUT_BUF_SIZE];
 
 if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-{		
+{
 write(1, buf, i);
 i = 0;
 }
